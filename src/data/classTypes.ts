5e2ce0bb576a88133a4e413c8125955f0b775482
tecxh <techno.zelda@gmail.com>
2025-12-04 14:17:30 -0500

    starter types and data

---
+++ src/data/classTypes.ts
@@ -0,0 +1,18 @@
+import { type ClassType, ClassTypes } from "../types";
+
+export const Caster: ClassType = {
+    type: ClassTypes.Caster,
+    label: 'Caster',
+    desc: 'A manipulator of the arcane and wild energies of the world.'
+}
+
+export const Warrior: ClassType = {
+    type: ClassTypes.Warrior,
+    label: 'Warrior',
+    desc: 'A skilled weapons wielder, capable to charging the fray.'
+}
+
+export const originalClasses = [
+    Caster,
+    Warrior
+]
