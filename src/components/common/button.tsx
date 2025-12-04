48c5fe00c16200380cc522c10b4de9dc8ee85073
tecxh <techno.zelda@gmail.com>
2025-12-04 14:18:42 -0500

    simple button

---
+++ src/components/common/button.tsx
@@ -0,0 +1,10 @@
+interface ButtonProps {
+    onClick: () => void;
+    label: string;
+}
+
+export const Button = ({ onClick, label }: ButtonProps) => {
+    return (
+        <button onClick={onClick}>{label}</button>
+    )
+}
