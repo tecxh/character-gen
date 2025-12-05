da95f629653457049e3224c7422012718109df5a
tecxh <techno.zelda@gmail.com>
2025-12-05 15:15:31 -0500

    add common screen & trivial styles

--- src/components/GeneratorMenu.tsx
+++ src/components/GeneratorMenu.tsx
@@ -1,10 +1,13 @@
+import { CommonScreen } from "./menuScreens/CommonScreen"
 import { OriginLocationScreen } from "./menuScreens/OriginLocationScreen"
 
 export const GeneratorMenu = () => {
     
     return (
         <div>
+            <CommonScreen>
             <OriginLocationScreen />
+            </CommonScreen>
         </div>
     )
 }
