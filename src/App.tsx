517ea6a36138e7c955e6b70339532c6cd6f9abc5
tecxh <techno.zelda@gmail.com>
2025-12-06 16:21:16 -0500

    add stepper logic & navigation menu on top to skip to specific points in generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import './App.css'
 import { GeneratorMenu } from './components/GeneratorMenu'
 import { CharacterProvider } from './lib/CharacterContext'
 import { CharacterInfoDisplay } from './components/CharacterInfoDisplay'
 import { DataProvider } from './lib/DataContext'
+import { GeneratorNavigation } from './components/GeneratorNavigation'
 
 function App() {
+    const [step, setStep] = useState(1);
+  
+    const increaseStep = () => setStep(step + 1);
+    const decreaseStep = () => setStep(step - 1);
+
 
   return (
     <Fragment>
       <DataProvider>
         <CharacterProvider>
+          <GeneratorNavigation setStep={setStep} />
           <div id="container">
             <div>
-              <GeneratorMenu />
+              <GeneratorMenu step={step} increaseStep={increaseStep} decreaseStep={decreaseStep} />
             </div>
             <div>
               <p>character display</p>
--- src/components/GeneratorMenu.tsx
+++ src/components/GeneratorMenu.tsx
@@ -1,13 +1,41 @@
 import { CommonScreen } from "./menuScreens/CommonScreen"
 import { OriginLocationScreen } from "./menuScreens/OriginLocationScreen"
+import { Button } from "./common";
 
-export const GeneratorMenu = () => {
-    
-    return (
-        <div>
-            <CommonScreen>
-            <OriginLocationScreen />
-            </CommonScreen>
+interface GeneratorMenuProps {
+  step: number;
+  increaseStep: () => void;
+  decreaseStep: () => void;
+}
+
+export const GeneratorMenu = ({ step, increaseStep, decreaseStep }: GeneratorMenuProps) => {
+  const menuContent = (() => {
+    switch (step) {
+      case 1:
+        return <OriginLocationScreen />;
+      case 2:
+        return <p>origin - class</p>
+      case 3:
+        return <p>deity - choose deity</p>
+      case 4:
+        return <p>deity - choose abilities</p>
+      default:
+        // error??
+        return null;
+    }
+  })()
+
+  return (
+    <div>
+      <CommonScreen>
+        {menuContent}
+        <div style={{ marginTop: '4rem' }}>
+          {step !== 1 && (
+            <Button label="previous" onClick={decreaseStep} />
+          )}
+          <Button label="next" onClick={increaseStep} />
         </div>
-    )
+      </CommonScreen>
+    </div>
+  )
 }
