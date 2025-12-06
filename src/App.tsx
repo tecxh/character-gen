fb967a748303343205a0d7c558b54836c234af2b
tecxh <techno.zelda@gmail.com>
2025-12-06 17:35:47 -0500

    rehouse generator logic into new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { Fragment, useState } from 'react'
 import './App.css'
-import { GeneratorMenu } from './components/GeneratorMenu'
-import { CharacterProvider } from './lib/CharacterContext'
-import { CharacterInfoDisplay } from './components/CharacterInfoDisplay'
-import { DataProvider } from './lib/DataContext'
-import { GeneratorNavigation } from './components/GeneratorNavigation'
+import { CharacterGenerator } from './components/CharacterGenerator'
 
 function App() {
-    const [step, setStep] = useState(1);
-  
-    const increaseStep = () => setStep(step + 1);
-    const decreaseStep = () => setStep(step - 1);
-
-
   return (
-    <Fragment>
-      <DataProvider>
-        <CharacterProvider>
-          <GeneratorNavigation setStep={setStep} />
-          <div id="container">
-            <div>
-              <GeneratorMenu step={step} increaseStep={increaseStep} decreaseStep={decreaseStep} />
-            </div>
-            <div>
-              <p>character display</p>
-            </div>
-            <div>
-              <CharacterInfoDisplay />
-            </div>
-          </div>
-        </CharacterProvider>
-      </DataProvider>
-    </Fragment>
+      <CharacterGenerator />
   )
 }
 
---
+++ src/components/CharacterGenerator/CharacterGenerator.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { CharacterProvider } from "../../lib/CharacterContext"
+import { DataProvider } from "../../lib/DataContext"
+import { GeneratorNavigation } from "../GeneratorNavigation";
+import { GeneratorMenu } from "../GeneratorMenu";
+import { CharacterInfoDisplay } from "../CharacterInfoDisplay";
+
+export const CharacterGenerator = () => {
+  const [step, setStep] = useState(1);
+
+  const increaseStep = () => setStep(step + 1);
+  const decreaseStep = () => setStep(step - 1);
+
+  return (
+    <DataProvider>
+      <CharacterProvider>
+        <GeneratorNavigation setStep={setStep} />
+        <div id="container">
+          <div>
+            <GeneratorMenu step={step} increaseStep={increaseStep} decreaseStep={decreaseStep} />
+          </div>
+          <div>
+            <p>character display</p>
+          </div>
+          <div>
+            <CharacterInfoDisplay />
+          </div>
+        </div>
+      </CharacterProvider>
+    </DataProvider>
+  )
+}
