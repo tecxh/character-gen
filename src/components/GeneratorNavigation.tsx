3f81f67de32a07623cca03fc469130b007023610
alecx <106361732+tecxh@users.noreply.github.com>
2025-12-06 16:25:54 -0500

    add stepper logic & generator navigation (#6)

---
+++ src/components/GeneratorNavigation.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./common"
+
+interface GeneratorNavigationProps {
+    setStep: (step: number) => void;
+}
+
+export const GeneratorNavigation = ({ setStep }: GeneratorNavigationProps) => {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'row', columnGap: '1rem'}}>
+            <Button label="Origins" onClick={() => setStep(1)} />
+            <Button label="Deity" onClick={() => setStep(3)} />
+        </div>
+    )
+}
