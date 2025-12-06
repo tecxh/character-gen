b51a121cd0a5d7a4afefafda6a3c1024735f5fc8
tecxh <techno.zelda@gmail.com>
2025-12-06 17:06:25 -0500

    show character class in info panel

--- src/components/CharacterInfoDisplay.tsx
+++ src/components/CharacterInfoDisplay.tsx
@@ -3,16 +3,17 @@ import { useData } from "../lib/DataContext";
 
 export const CharacterInfoDisplay = () => {
     const character = useCharacter();
-    const { locations } = useData();
+    const { locations, classes } = useData();
 
-    const { bio: { originLocation } } = character;
+    const { bio: { originLocation }, class: charClass } = character;
     const location = locations.find((location) => location.key === originLocation);
+    const selectedClass = classes.find((originalClass) => originalClass.key === charClass)
     
     return (
         <div>
             <p>{character.name}</p>
-            <p>{character.class}</p>
-            <p>{location?.personLabel}</p>
+            <span>{location?.personLabel + ' '}</span>
+            <span>{selectedClass?.label}</span>
         </div>
     )
 }
