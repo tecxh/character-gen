bfe5de10e7b9a876b4690805026cd037c8d35910
tecxh <techno.zelda@gmail.com>
2025-12-18 17:52:36 -0500

    install base UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import { CharacterGenerator } from './components/CharacterGenerator'
 
+// set isolation for BaseUI
+
 function App() {
   return (
+    <div style={{ isolation: 'isolate' }}>
       <CharacterGenerator />
+    </div>
   )
 }
 
